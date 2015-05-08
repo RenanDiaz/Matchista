bd140a390875773f7e15716daead3819f918863e
RenanDiaz <renandiazreyes@Nanchos-iMac.local>
2015-05-08 07:44:42 -0500

    Initial Commit

---
+++ Matchista/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchista
+//
+//  Created by Renán Díaz Reyes on 5/8/15.
+//  Copyright (c) 2015 Renán Díaz Reyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchista/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchista
+//
+//  Created by Renán Díaz Reyes on 5/8/15.
+//  Copyright (c) 2015 Renán Díaz Reyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
