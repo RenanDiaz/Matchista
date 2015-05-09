d0fc3dddeb76438af0f52262f5100229f1b02890
RenanDiaz <renandiazreyes@Nanchos-iMac.local>
2015-05-09 11:41:41 -0500

    Cards' images, flip counter & flip event

---
+++ Matchista/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchista
+//
+//  Created by Renán Díaz Reyes on 5/9/15.
+//  Copyright (c) 2015 Renán Díaz Reyes. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
