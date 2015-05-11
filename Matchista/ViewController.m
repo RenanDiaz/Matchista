9ee7a440d8b370b1e911a811855dca6481592663
RenanDiaz <renandiazreyes@Nanchos-iMac.local>
2015-05-10 22:15:42 -0500

    Matching 2 cards

--- Matchista/ViewController.m
+++ Matchista/ViewController.m
@@ -8,20 +8,21 @@
 
 #import "ViewController.h"
 #import "PlayingCardDeck.h"
-#import "PlayingCard.h"
+#import "CardMatchingGame.h"
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
-@property (nonatomic) int flipCount;
-@property (nonatomic) Deck *deck;
+@property (strong, nonatomic) CardMatchingGame *game;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @end
 
 @implementation ViewController
 
-- (Deck *)deck
+- (CardMatchingGame *)game
 {
-    if (!_deck) _deck = [self createDeck];
-    return _deck;
+    if (!_game) _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
+                                                          usingDeck:[self createDeck]];
+    return _game;
 }
 
 - (Deck *)createDeck
@@ -29,36 +30,36 @@
     return [[PlayingCardDeck alloc] init];
 }
 
-- (void)setFlipCount:(int)flipCount
-{
-    _flipCount = flipCount;
-    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
-}
 - (IBAction)touchCardButton:(UIButton *)sender
 {
-    if ([self.deck count]) {
-        if ([sender.currentTitle length])
-        {
-            [sender setBackgroundImage:[UIImage imageNamed:@"backside"]
-                              forState:UIControlStateNormal];
-            [sender setTitle:@"" forState:UIControlStateNormal];    }
-        else
-        {
-            Card *card = [self.deck drawRandomCard];
-            [sender setBackgroundImage:[UIImage imageNamed:@"frontside"]
-                              forState:UIControlStateNormal];
-            [sender setTitle:card.contents forState:UIControlStateNormal];
-            self.flipCount++;
+    NSUInteger chosenButtonIndex = [self.cardButtons indexOfObject:sender];
+    [self.game chooseCardAtIndex:chosenButtonIndex];
+    [self updateUI];
+}
+
+- (void)updateUI
+{
+    for (UIButton *cardButton in self.cardButtons) {
+        NSUInteger cardButtonIndex = [self.cardButtons indexOfObject:cardButton];
+        Card *card = [self.game cardAtIndex:cardButtonIndex];
+        [cardButton setTitle:[self titleForCard:card] forState:UIControlStateNormal];
+        if ([[self titleForCard:card] containsString:@"♥︎"] || [[self titleForCard:card] containsString:@"♦︎"]) {
+            [cardButton setTitleColor:[UIColor colorWithRed:255/255.0 green:0/255.0 blue:0/255.0 alpha:1.0] forState:UIControlStateNormal];
         }
+        [cardButton setBackgroundImage:[self backgroundImageForCard:card] forState:UIControlStateNormal];
+        cardButton.enabled = !card.isMatched;
+        self.scoreLabel.text = [NSString stringWithFormat:@"Score: %ld", (long)self.game.score];
     }
-    else
-    {
-        [sender setBackgroundImage:[UIImage imageNamed:@"emptycard"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"⌀" forState:UIControlStateNormal];
-        sender.enabled = NO;
-        
-    }
+}
+
+- (NSString *)titleForCard:(Card *)card
+{
+    return card.isChosen ? card.contents : @"";
+}
+
+- (UIImage *)backgroundImageForCard:(Card *)card
+{
+    return [UIImage imageNamed:card.isChosen ? @"frontside" : @"backside"];
 }
 
 @end
